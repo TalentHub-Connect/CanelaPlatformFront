ad326482c2036c3662795bacced58f7bbb5e9ad6
paula_penuela <paula.penuela@procontacto.com.mx>
2024-05-15 19:22:49 -0500

    Final

--- src/app/service/suscripcio.service.ts
+++ src/app/service/suscripcio.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cupon } from '../model/cupon';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { SuscripcionDto } from '../model/suscripcion-dto';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SuscripcioService {
+  private baseUrl = environment.URLSUSCRIPCION;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllCoupons(): Observable<SuscripcionDto[]> {
+    return this.http.get<SuscripcionDto[]>(this.baseUrl);
+  }
+
+  getCouponById(id: number): Observable<SuscripcionDto> {
+    return this.http.get<SuscripcionDto>(`${this.baseUrl}/${id}`);
+  }
+
+  createCoupon(coupon: SuscripcionDto): Observable<SuscripcionDto> {
+    return this.http.post<SuscripcionDto>(this.baseUrl, coupon);
+  }
+
+  updateCoupon(id: number, coupon: SuscripcionDto): Observable<SuscripcionDto> {
+    return this.http.put<SuscripcionDto>(`${this.baseUrl}/${id}`, coupon);
+  }
+
+  deleteCoupon(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
 }
