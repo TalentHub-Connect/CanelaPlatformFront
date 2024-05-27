04df1ddf56c0fb471d7ac7e5f9c19b87ec622ca5
Juan Felipe Arias <juanfeariascastillo@live.com>
2024-05-27 17:32:04 -0500

    Update suscripcio.service.ts

--- src/app/service/suscripcio.service.ts
+++ src/app/service/suscripcio.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Cupon } from '../model/cupon';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { SuscripcionDto } from '../model/suscripcion-dto';
 
