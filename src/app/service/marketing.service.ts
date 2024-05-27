9a211bfd3f9aa2c97582ee2e9ff46818200264a2
Wilermo <william.dmo.sec@gmail.com>
2024-05-26 19:17:25 -0500

    Arreglo URLS

--- src/app/service/marketing.service.ts
+++ src/app/service/marketing.service.ts
@@ -12,23 +12,23 @@ export class MarketingService {
   constructor(private http: HttpClient) {}
 
   findAll() {
-    return this.http.get<MarketingDto[]>("https://canelaapigatewayback-qa.up.railway.app/possibleclient/list");
+    return this.http.get<MarketingDto[]>("https://canelamarketingmicroservice-qa.up.railway.app/possibleclient/list");
   }
 
   findAllStatuses() {
-    return this.http.get<Marketingstatus[]>("https://canelaapigatewayback-qa.up.railway.app/marketingstatus/list");
+    return this.http.get<Marketingstatus[]>("https://canelamarketingmicroservice-qa.up.railway.app/marketingstatus/list");
   }
 
   saveAll(empresas: MarketingDto[]) {
-    return this.http.post<MarketingDto[]>("https://canelaapigatewayback-qa.up.railway.app/possibleclient/saveAll", empresas);
+    return this.http.post<MarketingDto[]>("https://canelamarketingmicroservice-qa.up.railway.app/possibleclient/saveAll", empresas);
   }
 
   saveStatus(estado: Marketingstatus) {
-    return this.http.post<Marketingstatus>("https://canelaapigatewayback-qa.up.railway.app/marketingstatus/save", estado);
+    return this.http.post<Marketingstatus>("https://canelamarketingmicroservice-qa.up.railway.app/marketingstatus/save", estado);
   }
 
   delete(id: number) {
-    return this.http.delete(`https://canelaapigatewayback-qa.up.railway.app/marketingstatus/delete/${id}`);
+    return this.http.delete(`https://canelamarketingmicroservice-qa.up.railway.app/marketingstatus/delete/${id}`);
 
   }
 }
