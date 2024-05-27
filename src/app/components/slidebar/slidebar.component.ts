bf1d8ea67628f315be4e7920dac60aaa2182d0db
paula_penuela <paula.penuela@procontacto.com.mx>
2024-05-27 14:57:22 -0500

    Correcciones

--- src/app/components/slidebar/slidebar.component.ts
+++ src/app/components/slidebar/slidebar.component.ts
@@ -7,9 +7,9 @@ import { SlidebarService } from 'src/app/service/slidebar.service';
   templateUrl: './slidebar.component.html',
   styleUrls: ['./slidebar.component.css'],
 })
-export class SlidebarComponent  implements OnInit {
+export class SlidebarComponent implements OnInit {
   nombreEmpresa: string = '';
-  modulos: { id: number, description: string, rolId: number, status: string }[] = [];
+
   roles: string[] = [];
 
   showAdminCard: boolean = false;
@@ -17,8 +17,7 @@ export class SlidebarComponent  implements OnInit {
   showMarketingCard: boolean = false;
   showCountCard: boolean = false;
 
-
-  constructor(private sidebarService: SidebarService) { }
+  constructor(private sidebarService: SidebarService) {}
 
   ngOnInit(): void {
     const roleStr = localStorage.getItem('role');
@@ -31,78 +30,33 @@ export class SlidebarComponent  implements OnInit {
       console.error('Role ID not found in localStorage');
     }
 
-    const hamBurger = document.querySelector(".toggle-btn") as HTMLElement;
-    hamBurger.addEventListener("click", () => {
-      const sidebar = document.querySelector("#sidebar") as HTMLElement;
-      sidebar.classList.toggle("expand");
+    const hamBurger = document.querySelector('.toggle-btn') as HTMLElement;
+    hamBurger.addEventListener('click', () => {
+      const sidebar = document.querySelector('#sidebar') as HTMLElement;
+      sidebar.classList.toggle('expand');
     });
 
-    const uniqueModules = new Set(); // Usar un Set para asegurar la unicidad
-    let pendingRequests = this.roles.length; // Contador para solicitudes pendientes
-
-    this.roles.forEach(role => {
-      const roleId = this.getRoleId(role);
-      if (roleId !== 0) { // Asegurarse de que el roleId es válido
-        this.sidebarService.obtenerModulos(roleId).subscribe(
-          (modulos: any[]) => {
-            console.log(`Modulos recibidos del API para el rol ${role}:`, modulos);
-
-            const filteredModulos = modulos.filter(modulo =>
-              modulo.rolId === roleId &&
-              modulo.status === 'ACTIVO' &&
-              !uniqueModules.has(modulo.id)
-            );
-
-            filteredModulos.forEach(modulo => uniqueModules.add(modulo.id));
-            this.modulos = [...this.modulos, ...filteredModulos];
-            console.log('Modulos filtrados:', this.modulos);
-
-            pendingRequests--;
-            if (pendingRequests === 0) {
-              this.setCardVisibility();
-            }
-          },
-          (error) => {
-            console.error('Error al obtener los módulos:', error);
-            pendingRequests--;
-            if (pendingRequests === 0) {
-              this.setCardVisibility();
-            }
-          }
-        );
-      } else {
-        pendingRequests--;
-        if (pendingRequests === 0) {
-          this.setCardVisibility();
-        }
-      }
-    });
+    this.updateSidebarVisibility();
   }
 
   getRoleId(roleName: string): number {
     const roleMap: { [key: string]: number } = {
-      'ADMIN': 1,
-      'SOPORTE': 7,
-      'MARKETING': 8,
-      'CUENTAS': 9
+      ADMIN: 1,
+      SOPORTE: 7,
+      MARKETING: 8,
+      CUENTAS: 9,
     };
     return roleMap[roleName] || 0;
   }
 
-  setCardVisibility(): void {
-    console.log('Setting card visibility based on modulos:', this.modulos);
-
-    const isAdmin = this.modulos.some(modulo => modulo.description === 'ADMIN');
-    this.showAdminCard = isAdmin
-    this.showSupportCard =isAdmin|| this.modulos.some(modulo => modulo.description === 'SOPORTE');
-    this.showMarketingCard = isAdmin||this.modulos.some(modulo => modulo.description === 'MARKETING');
-    this.showCountCard = isAdmin|| this.modulos.some(modulo => modulo.description === 'COUNT');
-
-
-    console.log('Card visibility - Admin:', this.showAdminCard);
-    console.log('Card visibility - Support:', this.showSupportCard);
-    console.log('Card visibility - Marketing:', this.showMarketingCard);
-    console.log('Card visibility - Nomina:', this.showCountCard);
+  updateSidebarVisibility(): void {
+    this.showAdminCard = this.roles.includes('ADMIN_CANELA');
+    this.showSupportCard =
+      this.roles.includes('SOPORTE') || this.roles.includes('ADMIN_CANELA');
+    this.showMarketingCard =
+      this.roles.includes('MARKETING') || this.roles.includes('ADMIN_CANELA');
+    this.showCountCard =
+      this.roles.includes('CUENTAS') || this.roles.includes('ADMIN_CANELA');
   }
 
   logout(): void {
